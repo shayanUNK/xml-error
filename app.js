046255655988f99aaf6917c2cb8726558e19590d
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-12-31 18:12:59 +0330

    abcd
    abcd

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let error200 = document.querySelector("#error200");
+let error400 = document.querySelector("#error400");
+let error500 = document.querySelector("#error500");
 let output = document.querySelector("#output");
 
 error200.addEventListener("click", submitOutput);
@@ -11,11 +13,29 @@ function submitOutput() {
   xhr.onload = function () {
     if (this.status === 200) {
       output.innerHTML = this.response;
-    } else if (this.status >= 400 && this.status < 500) {
-      console.error("error 404");
-    } else if (this.status >= 500) {
-      console.error("error 504");
     }
   };
+  xhr.send();
+}
+
+error400.addEventListener("click", clientError);
+
+function clientError() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/ucesful.txt");
+  if (this.status >= 400 && this.status < 500) {
+    console.error("client error");
+  }
+  xhr.send();
+}
+
+error500.addEventListener("click", clientError);
+
+function clientError() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://httpstat.us/500");
+  if (this.status >= 500) {
+    alert("server error");
+  }
   xhr.send();
 }
