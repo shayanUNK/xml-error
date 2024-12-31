6c61b931545feb6993b804b4d4d7fcabbeae3515
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-12-31 16:12:48 +0330

    feat: xhr, add: sweet alert
    add seet alert file.
    feat partly of xhr

---
+++ app.js
@@ -0,0 +1,10 @@
+import Swal from "sweetalert2";
+
+let submit = document.querySelector("#error200");
+submit.addEventListener("click", submitOutput);
+
+function submitOutput() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "");
+}
