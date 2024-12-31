671dcdd2eb311853f4fad96f64ca8aa2fa530dd4
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-12-31 16:24:59 +0330

    feat: xhr
    complete xhr code

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import Swal from "sweetalert2";
+let error200 = document.querySelector("#error200");
+let output = document.querySelector("#output");
 
-let submit = document.querySelector("#error200");
-submit.addEventListener("click", submitOutput);
+error200.addEventListener("click", submitOutput);
 
 function submitOutput() {
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "");
+  xhr.open("GET", "/successful.txt");
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      output.innerHTML = this.response;
+    } else if (this.status >= 400 && this.status < 500) {
+      console.error("error 404");
+    } else if (this.status >= 500) {
+      console.error("error 504");
+    }
+  };
+  xhr.send();
 }
